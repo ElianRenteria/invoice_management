20fbae37fd85dd87961f6335b8fbd6f7aea698ec
ElianRenteria <elianrenteriadevelopment@gmail.com>
2024-05-06 12:37:05 -0700

    button to test api

---
+++ frontend/src/components/apiTest.vue
@@ -0,0 +1,29 @@
+<template>
+  <Button label="Click" @click="makeAPICall" />
+</template>
+
+<script lang="ts">
+import Button from "primevue/button";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "apiTest",
+  components: {
+    Button,
+  },
+  props: {
+    msg: String,
+  },
+  methods: {
+    async makeAPICall() {
+      try {
+        const response = await fetch('https://api.example.com/data');
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error('Error making API call:', error);
+      }
+    }
+  }
+});
+</script>
