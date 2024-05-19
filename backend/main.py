0119f3eeba7c3ada0345ff8014adf60ed4babd8c
Salman Burhan <salmanfburhan@gmail.com>
2024-05-19 16:47:14 -0700

    refactor: Remove reload option in uvicorn.run() call
    Removed the reload option in uvicorn.run() call to prevent automatic server
    reloading. This option is not possible when launching using a an app instance over an import string.

--- backend/main.py
+++ backend/main.py
@@ -23,4 +23,4 @@ def create_app():
 if __name__ == '__main__':
     import uvicorn
     app = create_app()
-    uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
