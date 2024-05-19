6888c6706a78cb1801100af49e8ba05dcde495e0
Salman Burhan <salmanfburhan@gmail.com>
2024-05-19 16:35:11 -0700

    feat: Refactor start_application function to create_app
    Refactored the start_application function to create_app in order to
    improve code readability and follow best practices. Updated the main.py
    file to call create_app function when running the application.

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ def include_router(app):
     app.include_router(pages_router)
 
 
-def start_application():
+def create_app():
     app = FastAPI(title="invoiceManager", version=0.1)
     app.add_middleware(
         CORSMiddleware,
@@ -20,4 +20,7 @@ def start_application():
     return app
 
 
-app = start_application()
+if __name__ == '__main__':
+    import uvicorn
+    app = create_app()
+    uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
