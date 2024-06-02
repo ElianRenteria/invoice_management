e3d94f96b803b4700f92dac8b11f993e1c58c499
Elian Renteria <elianrenteriadevelopment@gmail.com>
2024-06-02 10:28:21 -0700

    side panel nav finished

--- frontend/src/components/SidePanelNav.vue
+++ frontend/src/components/SidePanelNav.vue
@@ -2,7 +2,10 @@
     <div>
       <nav>
         <router-link to="/dashboard">Dashboard</router-link>
-        <router-link to="/profile">Profile</router-link>
+        <router-link to="/clients">Clients</router-link>
+        <router-link to="/invoices">Invoices</router-link>
+        <router-link to="/services">Services</router-link>
+        <router-link to="/settings">Settings</router-link>
       </nav>
     </div>
   </template>
