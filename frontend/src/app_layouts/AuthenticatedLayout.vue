d14833733eef9dd3f7fe65d07da074c682e2580c
Elian Renteria <elianrenteriadevelopment@gmail.com>
2024-06-17 18:16:55 -0700

    fixed setup

--- frontend/src/app_layouts/AuthenticatedLayout.vue
+++ frontend/src/app_layouts/AuthenticatedLayout.vue
@@ -10,7 +10,7 @@
   export default {
     name: "AuthenticatedLayout",
     components: {
-      SidePanel,
+      SidePanelNav,
     },
   };
   </script>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <Menubar :model="items" />
+    <nav class="menu-bar">
+      <h1>Invoice Manager</h1>
+      <router-link to="/" class="menu-item">Home</router-link>
+      <router-link to="/about" class="menu-item">About</router-link>
+      <router-link to="/login" class="menu-item">Log In</router-link>
+      <router-link to="/signup" class="menu-item">Sign Up</router-link>
+    </nav>
+  </template>
+  
+  <script>
+  import Menubar from 'primevue/menubar';
+  export default {
+    name: "NavBar",
+    components: {
+      Menubar
+    },
+    data: function() {
+      return {
+        
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .menu-bar {
+    background-color: #1976d2; /* Set background color */
+    color: #ffffff; /* Set text color */
+    padding: 10px; /* Add padding */
+  }
+  
+  .menu-item {
+    text-decoration: none; /* Remove underline from links */
+    color: #ffffff; /* Set text color */
+    margin-right: 10px; /* Add margin between menu items */
+  }
+  
+  .menu-item:hover {
+    text-decoration: underline; /* Add underline on hover */
+  }
+  </style>
