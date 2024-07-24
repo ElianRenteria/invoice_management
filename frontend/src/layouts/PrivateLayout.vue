ae771c89ad851905e5569ce00364a91bcc81eff3
Salman Burhan <salmanfburhan@gmail.com>
2024-07-23 20:16:11 -0700

    🎨 Adjust sidebar and avatar styles for the aesthetics.

--- frontend/src/layouts/PrivateLayout.vue
+++ frontend/src/layouts/PrivateLayout.vue
@@ -1,18 +1,25 @@
 <template>
-  <div
-    class="flex justify-content-between align-items-center p-5"
-    :style="{ width: '100dvw' }"
-  >
-    <h1>Invoice Management</h1>
-    <div class="flex flex-row align-items-center gap-3">
-      <Button icon="pi pi-bell" rounded />
-      <Avatar label="SB" :size="'large'" />
+  <div class="container">
+    <div class="flex justify-content-between px-5" :style="{ width: '100dvw' }">
+      <h1>Invoice Management</h1>
+      <div class="flex flex-row align-items-center gap-3">
+        <Button :size="'large'" icon="pi pi-bell" rounded />
+        <Avatar
+          label="SB"
+          :size="'large'"
+          :shape="'circle'"
+          :class="['surface-section', 'text-color-secondary']"
+        />
+      </div>
     </div>
-  </div>
-  <div class="flex flex-row justify-content-start gap-0">
-    <router-view name="sidebar" />
-    <div class="container w-full">
-      <router-view />
+    <div
+      class="flex justify-content-start align-items-stretch gap-0"
+      style="height: calc(100vh - 6rem)"
+    >
+      <router-view name="sidebar" />
+      <router-view
+        class="w-full surface-ground border-round-3xl border-noround-right p-3"
+      />
     </div>
   </div>
   <!-- <div
@@ -28,8 +35,10 @@
 
 <style lang="css">
 .container {
-  border-radius: 25px;
-  background-color: white;
-  align-items: stretch;
+  height: 100dvh;
 }
+/* .container {
+  background-color: white;
+  border-radius: 4em 0 0 4em;
+} */
 </style>
