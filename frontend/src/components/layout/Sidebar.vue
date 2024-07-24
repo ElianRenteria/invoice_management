e76a3af62a7bb1d70ee505670c018f5e49047513
Salman Burhan <salmanfburhan@gmail.com>
2024-07-23 20:16:11 -0700

    🎨 Adjust sidebar and avatar styles for the aesthetics.

--- frontend/src/components/layout/Sidebar.vue
+++ frontend/src/components/layout/Sidebar.vue
@@ -1,43 +1,69 @@
 <template>
   <div
-    class="flex flex-column justify-content-center p-3 gap-3"
-    :style="{ width: '15rem', height: '100dvh' }"
+    class="sidebar flex flex-column justify-content-center p-5 gap-3"
+    :style="{ width: '10rem' }"
   >
     <span class="text-sm text-color-secondary uppercase mb-3">Menu</span>
-    <router-link to="/dashboard" class="nav-link">
-      <Button icon="pi pi-home" rounded outlined />
+    <router-link to="/dashboard" class="nav-link active">
+      <!-- <Button icon="pi pi-home" :size="'large'" rounded outlined /> -->
+      <i class="pi pi-home"></i>
+      <span>Home</span>
     </router-link>
     <router-link to="/invoices" class="nav-link">
-      <Button icon="pi pi-receipt" rounded outlined />
+      <i class="pi pi-receipt"></i>
+      <span>Invoices</span>
     </router-link>
     <router-link to="/profile" class="nav-link">
-      <Button icon="pi pi-user" rounded outlined />
-    </router-link>
-    <router-link to="/settings" class="nav-link">
-      <Button icon="pi pi-cog" rounded outlined />
+      <i class="pi pi-user"></i>
+      <span>Profile</span>
     </router-link>
     <div class="flex-grow-1"></div>
-    <Button label="Logout" icon="pi pi-sign-out" severity="secondary" />
+    <router-link to="/settings" class="nav-link">
+      <i class="pi pi-cog"></i>
+      <span>Settings</span>
+    </router-link>
+    <router-link to="/logout" class="nav-link">
+      <i class="pi pi-sign-out"></i>
+      <span>Logout</span>
+    </router-link>
   </div>
 </template>
 
 <style lang="css" scoped>
+.sidebar:hover {
+  /* TODO: Implement Some Sort of Expandable Sidebar and Toggle Span Display */
+}
+
 .nav-link {
-  color: var(--primary-color);
+  color: var(--text-color-secondary);
   text-decoration: none;
   align-self: center;
-  height: 2.25rem;
   display: flex;
-  column-gap: 1rem;
-  font-size: 1.1rem;
+  flex-direction: row;
+  column-gap: 0.5rem;
+  width: 4rem;
+  height: 4rem;
+  padding: 1.25rem;
+  border-radius: 2rem;
+  transition: background-color 0.35s;
+}
+.nav-link.active {
+  color: var(--primary-color-reverse);
+  background-color: var(--surface-100);
+}
+.nav-link:hover {
+  background-color: var(--surface-100);
+  color: var(--primary-color);
 }
 
-.nav-link:hover {
-  color: white;
-  text-decoration: none;
+.nav-link span {
+  display: none;
 }
+/* .nav-link:hover span {
+  display: inline;
+} */
 
 .nav-link i {
-  font-size: 1.25rem;
+  font-size: 1.5rem;
 }
 </style>
