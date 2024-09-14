70175461ac505e79f7a3f99e10d47d021e6af753
elianrenteria <elianrenteriadevelopment@gmail.com>
2024-09-13 23:09:41 -0700

    reactive scheme for user setup

--- frontend/src/views/private/ProfileView.vue
+++ frontend/src/views/private/ProfileView.vue
@@ -1,7 +1,14 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+    const first_name = ref('');
+    const last_name = ref('');
+    const date_of_birth = ref('');
+    const email = ref('');
+</script>
 
 <template>
   <h1>Profile</h1>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+
+</style>
