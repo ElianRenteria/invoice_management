568fd3815c0ec30d452a34a9dd90bced7f65f707
Salman Burhan <salmanfburhan@gmail.com>
2024-09-19 19:58:31 -0700

    🍞 Add (partial) toast notifications for success/error on Invoice CRUD operations.
    Partially implemented. Invoice type must be fully implemented in order to complete proper toast notification functionality (referencing invoice within toast content).

--- frontend/src/composables/useInvoices.ts
+++ frontend/src/composables/useInvoices.ts
@@ -1,15 +1,34 @@
+import { useToastService } from "./useToastService";
 import { Invoice } from "../types";
 import HttpClient from "../utils/httpClient";
 
 export function useInvoices() {
   const client = new HttpClient();
+  const toast = useToastService();
 
   async function getInvoices() {
-    const response = await client.get("/invoices");
-    return response.data as Invoice[];
+    try {
+      const response = await client.get("/invoices");
+      if (response.status !== 200) {
+        throw new Error("Failed to load invoices.");
+      }
+      return response.data as Invoice[];
+    } catch (error: Error | any) {
+      toast.present("error", error, {
+        title: "Load Failed",
+      });
+    }
   }
 
   async function createInvoices(_invoice: Invoice) {
+    /**
+     * TODO: TOAST FUNCTIONALITY
+     * Implement toast messages for success and error cases.
+     * Use the `toast.present` function from the `useToastService` composable.
+     * Invoice type must be implemented prior to toast implementation.
+     * Currently, the Invoice type is defined, but not implemented.
+     */
+
     const response = await client.post("/invoices", _invoice);
     return response.data as Invoice;
   }
